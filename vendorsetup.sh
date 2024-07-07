701c50b5c674649fcd0d5bd0bb3d634fa9db0587
Meisie Widuri <ewprjkt@proton.me>
2024-07-07 19:44:30 +0700

    X00TD: Update vendorsetup

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,6 @@
 rm -rf kernel/asus/sdm660
 git clone --depth=1 --recursive https://github.com/Tiktodz/android_kernel_asus_sdm636 -b 14-eas-su kernel/asus/sdm660
-#rm -rf kernel/asus/sdm660/KernelSU/userspace
+rm -rf kernel/asus/sdm660/KernelSU/userspace
 
 rm -rf device/asus/X00TD
 git clone --depth=1 https://github.com/Tiktodz/android_device_asus_X00TD -b lineage-21-4.4 device/asus/X00TD
